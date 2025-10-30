c80bd73764e88f4b7b047618e0396bed49bc9004
Wilson Wolf Costa <wilson.w.costa@gmail.com>
2025-10-30 20:20:01 -0300

    final do cap2

--- aula/src/main/java/com/knight/estoque/servicos/ListagemLivros.java
+++ aula/src/main/java/com/knight/estoque/servicos/ListagemLivros.java
@@ -5,12 +5,9 @@ import java.util.List;
 import com.knight.estoque.daos.LivroDAO;
 import com.knight.estoque.modelos.Livro;
 
-import jakarta.jws.WebMethod;
 import jakarta.jws.WebResult;
 import jakarta.jws.WebService;
 import jakarta.xml.ws.Endpoint;
-import jakarta.xml.ws.RequestWrapper;
-import jakarta.xml.ws.ResponseWrapper;
 
 @WebService
 public class ListagemLivros {
@@ -21,11 +18,8 @@ public class ListagemLivros {
         return livroDAO.listarLivros();
     }
 
-    @RequestWrapper(className = "com.knight.estoque.servicos.jaxws.ListarLivrosPaginacao", localName = "listarLivrosPaginacao")
-    @ResponseWrapper(className = "com.knight.estoque.servicos.jaxws.ListarLivrosPaginacaoResponse", localName = "livrosPaginados")
     @WebResult(name = "livro")
-    @WebMethod(operationName = "listarLivrosPaginacao")
-    public List<Livro> listarLivros(Integer numeroDaPagina, Integer tamanhoDaPagina) {
+    public List<Livro> listarLivrosPaginacao(Integer numeroDaPagina, Integer tamanhoDaPagina) {
         LivroDAO livroDAO = obterDAO();
         return livroDAO.listarLivros(numeroDaPagina, tamanhoDaPagina);
     }
